3464a09b0de6d80a7fd0b37b4babb572ee923ae7
HansyToadsworth <daddytoadsworth17@gmail.com>
2026-02-27 16:17:52 -0500

    Moved requirements.txt to the root, and changed vercel.json and next.config.ts once again.

--- testrepo/next.config.ts
+++ testrepo/next.config.ts
@@ -3,14 +3,6 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
-  rewrites: async () => {
-    return [
-      {
-        source: '/api/flask/:path*',
-        destination: '/backend/app.py',
-      }
-    ]
-  }
 };
 
 export default nextConfig;
