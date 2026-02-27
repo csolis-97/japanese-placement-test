2e62b62ba153c6533972f513515c5c9f1b02b55f
HansyToadsworth <daddytoadsworth17@gmail.com>
2026-02-27 16:03:04 -0500

    Updated vercel.json and next.config.ts to see if the python routing gets fixed.

--- testrepo/next.config.ts
+++ testrepo/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
+  rewrites: async () => {
+    return [
+      {
+        source: '/api/flask/:path*',
+        destination: '/backend/app.py',
+      }
+    ]
+  }
 };
 
 export default nextConfig;
