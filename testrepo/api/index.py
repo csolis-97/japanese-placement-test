85101476e7bda15ef53fc88c0bc487c6f8bdfa31
HansyToadsworth <daddytoadsworth17@gmail.com>
2026-03-02 21:53:05 -0500

    Pulled out the app declaration from the try block

--- testrepo/api/index.py
+++ testrepo/api/index.py
@@ -10,16 +10,15 @@ import MySQLdb.cursors
 import os
 import datetime
 
+# Import all the functions defined elsewhere in the backend
+from test_data_functions import *
+from question_retrieval_fuctions import *
+from answer_storage_functions import *
+from test_submission_functions import *
+
+app = Flask(__name__)
+
 try:
-    # Import all the functions defined elsewhere in the backend
-    from test_data_functions import *
-    from question_retrieval_fuctions import *
-    from answer_storage_functions import *
-    from test_submission_functions import *
-
-
-    app = Flask(__name__)
-
     if os.getenv('VERCEL_URL'):
         frontURL = os.getenv('VERCEL_URL')
     else:
